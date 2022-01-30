e08a726b465ba72e82534d02afd13bc6a3422148
Akshay Gupta <akshaygupta.us@gmail.com>
2022-01-30 17:24:37 -0500

    wrote code to handle threading for handling clients, need to write code for actually client itself

--- server.c
+++ server.c
@@ -2,8 +2,9 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include <errno.h>
 #include <stdio.h>
@@ -12,16 +13,34 @@
 #define BACKLOG_NUMBER 5 // the number of connections allowed on queue for connect
 // ususally 5 or 10 is acceptable, system limits silently on 20
 
-// a sample call if you’re a server who wants to listen on
-// your host’s IP address, port 8080
-// just sets up structures to use later, no listening or network setup done
-void setup_hints_and_servinfo()
+void sigchild_handler(int s)
 {
+  // waitpid() might overwrite errno, so we save and restore it:
+  int saved_errno = errno;
+  while (waitpid(-1, NULL, WNOHANG) > 0)
+  {
+  };
+  errno = saved_errno;
+}
 
+void handle_client(int socket_fd)
+{
+}
+
+void web_server()
+{
   // setup general structs and stuff
   int status;
   struct addrinfo hints;
-  struct addrinfo *servinfo; // points to results
+  struct addrinfo *servinfo;      // points to results
+  struct sockaddr_in server_addr; // connector's address information
+  struct sockaddr_in host_addr;
+
+  socklen_t addr_len = sizeof(struct sockaddr_storage);
+  int yes = 1;
+
+  struct sigaction sa; // used for sigchild handling
+
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;     // we dont care to specify ipv4 or ipv6
   hints.ai_socktype = SOCK_STREAM; //tcp stream sockets
@@ -30,7 +49,7 @@ void setup_hints_and_servinfo()
   //  node is NULL because we are not connecting to an external host or IP
   if ((status = getaddrinfo(NULL, MY_PORT, &hints, &servinfo)) != 0)
   {
-    perror("setsockopt");
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
     exit(1);
   }
   // hints parameter points to a struct addrinfo that you’ve already
@@ -47,7 +66,6 @@ void setup_hints_and_servinfo()
   }
 
   //prevent address in use error by allowing reusal of socket
-  int yes = 1;
   if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
   {
     perror("setsockopt");
@@ -62,20 +80,58 @@ void setup_hints_and_servinfo()
     // “Address already in use.” means that a little socket is hogging port
   }
 
+  freeaddrinfo(servinfo); // free the linked-list since we don't need it now
+
   // start listening socket
   // if we were connecting to a remote host, use connect()
   // since we are using local host, we use listen()
-  if (listen(sockfd, BACKLOG_NUMBER))
+  if (listen(sockfd, BACKLOG_NUMBER) == -1)
   {
     perror("listen error");
     exit(1);
   }
 
-  while (1)
+  sa.sa_handler = sigchild_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1)
   {
+    perror("sigaction");
+    exit(1);
   }
 
-  freeaddrinfo(servinfo); // free the linked-list
+  int new_fd;
+  int pid;
+  printf("server: waiting for connections...\n");
+  while (1)
+  {
+    new_fd = accept(sockfd, (struct sockaddr *)&host_addr, &addr_len);
+    if (new_fd == -1)
+    {
+      perror("Accept");
+      exit(1);
+    }
+
+    pid = fork();
+    // we must fork and create a child process to handle connection
+    // parent process will continue waiting for new connections and creating new children
+    if (pid == -1)
+    {
+      perror("Fork");
+      exit(1);
+    }
+
+    if (pid == 0) // if we are the child
+    {
+      close(sockfd); // child doesn't need the listener
+      handle_client(new_fd);
+      exit(0);
+    }
+    else
+    {
+      close(new_fd);
+    }
+  }
 }
 
 int main(int argc, char *argv[])
